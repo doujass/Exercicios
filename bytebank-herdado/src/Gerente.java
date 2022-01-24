ac250dcc74bd00c1e6893634622308659e8ee6b1
Douglas <douglasbusiness.augusto@gmail.com>
2022-01-24 17:37:58 -0300

    Incluindo funcionarios no banco

---
+++ bytebank-herdado/src/Gerente.java
@@ -0,0 +1,9 @@
+public class Gerente extends FuncionarioAutenticavel {
+
+        public double getBonificacao() {
+            System.out.println("Chamando o método de bonificacao do Gerente");
+        return super.getSalario();
+    }
+
+
+}
---
+++ bytebank-herdado/src/SistemaInterno.java
@@ -0,0 +1,18 @@
+public class SistemaInterno extends Funcionario {
+
+    int senha = 2222;
+
+    public void autentica(FuncionarioAutenticavel g) {
+        boolean autenticou = g.autentica(this.senha);
+        if (autenticou) {
+            System.out.println("Pode entrar no sistema");
+        } else {
+            System.out.println("Não pode entrar no sistema");
+        }
+    }
+
+    @Override
+    public double getBonificacao() {
+        return 0;
+    }
+}
